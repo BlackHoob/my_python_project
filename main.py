b00c546a17213728cd8820b71f2dbd59900af6e2
Elie marc Kouakou <kouakoueliemarc@gmail.com>
2025-05-13 00:16:31 +0200

    Sauvegarde avant rebase

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 
 
-def hello():
- return "Hello, GitHub Actions!"
+# def hello (name="GitHub Actions "):
+#  return f"Hello, {name}!"
+
+# def hello(name="GitHub Actions"):
+#  if not isinstance(name,str):
+#     raise TypeError("Le nom doit êtreune chaîne")
+# raise ValueError("Erreur volontaire")
+
+def hello(firstname="John", lastname="Doe"):
+ return f"Hello, {firstname} {lastname}!"
--- test_main.py
+++ test_main.py
@@ -1,4 +1,17 @@
+import pytest
 from main import hello
+import time
+# def test_hello_type_error():
+#  with pytest.raises(TypeError):
+#     hello(123)
 
-def test_hello():
- assert hello() == "Hello, GitHub Actions!"
+
+# def test_hello_performance():
+#  start =time.time()
+#  for _ in range(1000):
+#     hello("EPSI")
+#  duration = time.time()- start
+#  assert duration < 1
+
+def test_hello_full_name():
+ assert hello("Jane", "Smith") == "Hello, Jane Smith!"
