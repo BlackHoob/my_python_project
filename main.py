8d7fe6149f3822323c1b2894a6d64dbe1a43c05e
Elie marc Kouakou <kouakoueliemarc@gmail.com>
2025-05-13 00:37:13 +0200

    Correction des erreurs flake8

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
 
 
-# def hello (name="GitHub Actions "):
-#  return f"Hello, {name}!"
-
-# def hello(name="GitHub Actions"):
-#  if not isinstance(name,str):
-#     raise TypeError("Le nom doit êtreune chaîne")
-# raise ValueError("Erreur volontaire")
-
-def hello(firstname="John", lastname="Doe"):
- return f"Hello, {firstname} {lastname}!"
+def hello (name="GitHub Actions "):
+ return f"Hello, {name}!"
--- test_main.py
+++ test_main.py
@@ -1,17 +1,6 @@
-import pytest
 from main import hello
-import time
-# def test_hello_type_error():
-#  with pytest.raises(TypeError):
-#     hello(123)
 
 
-# def test_hello_performance():
-#  start =time.time()
-#  for _ in range(1000):
-#     hello("EPSI")
-#  duration = time.time()- start
-#  assert duration < 1
 
-def test_hello_full_name():
- assert hello("Jane", "Smith") == "Hello, Jane Smith!"
+def test_hello_custom_name():
+    assert hello("EPSI") == "Hello, EPSI!"
